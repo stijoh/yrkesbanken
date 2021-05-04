94b2017cb88f93973c8b4502cf44596e27c3c88d
Stian Johansen <stian.a.johansen@gmail.com>
2021-05-04 18:53:48 +0200

    fixed a typo

--- db/migrate/20210411120303_create_yrkes.rb
+++ db/migrate/20210411120303_create_yrkes.rb
@@ -2,7 +2,7 @@ class CreateYrkes < ActiveRecord::Migration[6.1]
   def change
     create_table :yrkes do |t|
       t.string :navn
-      t.references :tema, null: true
+      t.references :tema, null: false, foreign_key: true 
 
       t.timestamps
     end
